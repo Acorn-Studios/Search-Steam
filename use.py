89ddcf253583bb670cfcf7aa45726010d1a4c769
Acorn-Studios <80981239+Acorn-Studios@users.noreply.github.com>
2025-06-01 11:09:46 -0500

    Fin API

--- use.py
+++ use.py
@@ -4,11 +4,8 @@ import csv
 
 class SteamTransformer:
     def __init__(self):
-        # Load a pretrained Sentence Transformer model
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
-        # Load the embeddings from the pickle file
         self.embeddings = pkl.load(open("embeddings.pkl", "rb"))
-        # Load our paired sentences with the appids
         csv_file = "descriptions_cleaned.csv"
         with open (csv_file, 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
@@ -24,9 +21,7 @@ class SteamTransformer:
         embedding = self.encode(prompt)
         similarities = self.similarity(embedding)
         index = similarities.argmax()
-        return {"data" : self.labeled[index], "appid": int(self.labeled[index][0]), "url" : "https://store.steampowered.com/app/" + str(self.labeled[index][0])}
-
-
+        return { "appid": int(self.labeled[index][0]), "url" : "https://store.steampowered.com/app/" + str(self.labeled[index][0]), "similar_row" : self.labeled[index]}
 
 # 4. Calculate the embedding similarities
 ST = SteamTransformer()
